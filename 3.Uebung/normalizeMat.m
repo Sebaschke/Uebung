62ed870ceee496dacc9bef6b6abfb6a5060d6a6e
Sebastian Maschke <Sebastian.Maschke@web.de>
2020-05-21 22:57:34 +0200

    Added Exercise 3

--- 3.Uebung/normalizeMat.m
+++ 3.Uebung/normalizeMat.m
@@ -1,20 +1,19 @@
 function [D] = normalizeMat(A, norm, type)
 % NORMALIZE_MAT - Äquilibrierung einer regulären Matrix
 %   Syntax:   [D] = normalizeMat(A, norm, type)
-%   Input:    A - Array (m x n Matrix)
+%   Input:    A - Array (n x n Matrix)
 %             norm - String der festlegt, bezüglich welcher Norm
 %             äquilibriert werden soll. Möglich ist 'infty' oder 'one'
 %             type - String, der die Art der Äquilibrierung festlegt
 %   Output:   D - Diagonalmatrix, welche die Matrix A äquilibriert
+ 
+[n,n] = size(A)     % A Matrix A regulär
 
-
-%% 
-% matrix norm = infty -> sum of rows 
+% matrix norm = infty -> sum of rows
 if strcmp(norm, 'infty')
     % ||DA||=1 
     if strcmp(type, 'rows')
-        D = diag(((A*ones(size(A,2),1)).^-1));
-        
+        D = diag(sum(abs(A),2).^-1)
     % ||AD||=1    
     elseif strcmp(type, 'cols')
         % bislang fehlerhaft
@@ -22,7 +21,6 @@ if strcmp(norm, 'infty')
         % a11*d1 + a12*d2 = 1 abhängt und somit nicht einfach lösbar ist
         D = diag((ones(1, size(A.', 1))*A.').^-1);
     end   
-    
 % matrix norm = one -> sum of columns        
 elseif strcmp(norm, 'one')
     % ||DA||=1
@@ -31,9 +29,8 @@ elseif strcmp(norm, 'one')
         % z.B. d1*a11 + d2*a21 = 1 und ist nicht einfach lösbar 
     % ||AD||=1
     elseif strcmp(type, 'cols')
-        D = diag(((ones(1, size(A,1))*A).^-1));
-    end
-    
+        D = diag(sum(abs(A)).^-1);
+    end 
 else
     fprintf('No valid argument for norm given\n')
 end
